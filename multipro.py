76cb7de482bf75a861d839bbe0c7764912c75e43
Dino Yeo Guo Wei <2502165@sit.singaporetech.edu.sg>
2025-10-03 15:57:36 +0800

    Solved syntax error for Multi Processor

--- multipro.py
+++ multipro.py
@@ -2,24 +2,23 @@ import time
 import multiprocessing
 
 def basic_func(x):
-        if x == 0:
-                return 'zero'
-        elif x%2 == 0:
-                return 'even'
-        else:
-                return 'odd'
+	if x == 0:
+		return 'even'
+	elif x%2 == 0:
+		return 'even'
+	else:
+		return 'odd'
 
 def multiprocessing_func(x):
-
-        y = x*x
-        time.sleep(2)
-        print('{} squared results in a/a {} number'.format(i, basic_func(y)))
+	y = x*x
+	time.sleep(2)
+	print('{} squared results in a/a {} number'.format(x, basic_func(y)))
 
 if __name__ == '__main__':
 	starttime = time.time()
 	processes = []
 	for i in range(0,10):
-		p = multiprocessing.Process(target=multiprocessing_func, arg=(i,))
+		p = multiprocessing.Process(target=multiprocessing_func, args=(i,))
 		processes.append(p)
 		p.start()
 
